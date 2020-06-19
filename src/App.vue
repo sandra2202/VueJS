eab9823ff4b3959936e988a75753225a6f5782a5
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-19 17:33:13 -0500

    Crear pagina para el detalle de un componente

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app">    
+  <div id="app"> 
+    <Header/>
     <List/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
 //import HelloWorld from './components/HelloWorld.vue'
 import List from './components/List'
 //import Detail from './components/Detail'
@@ -13,18 +15,8 @@ export default {
   name: 'App',
   components: {
     List,
+    Header
     //Detail
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark" class="mb-3">
+            <b-navbar-brand>Elements</b-navbar-brand>
+
+            <b-navbar-nav>
+                <b-nav-item href="#">Home</b-nav-item>
+                <b-nav-item-dropdown text="User" right>
+                    <b-dropdown-item href="#">Account</b-dropdown-item>
+                    <b-dropdown-item href="#">Settings</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>            
+        </b-navbar>
+    </div>
+</template>
