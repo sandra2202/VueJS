b0056d195d7334b68855a735f0749e6fbe3d51c1
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-19 15:25:49 -0500

    Consumir fetch desde Vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,13 +1,11 @@
 <template>
     <div>
 
-        <div class="box" v-for="e in elements" v-bind:key="e">
-            {{ e }}            
+        <div class="box" v-for="e in elements" v-bind:key="e.id">
+            {{ e.title }}            
         </div>
 
         <button v-on:click="findAll">Click</button>
-
-        {{ msj }}
 
     </div>
 </template>
@@ -17,14 +15,14 @@ export default {
     data(){
         return{
             msj: "... :(",
-            elements: ["Uno", "Dos", "Tres"]
+            elements: []
         };
     },
     methods: {
         findAll: function(){
             fetch('http://127.0.0.1:8000/api/element/?format=json') 
                 .then(res => res.json())
-                .then(res => console.log(res))
+                .then(res => this.elements = res)                
         }
     },
 }
