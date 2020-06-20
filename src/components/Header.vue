dd0103a9ed8936d21e9236470097ed8fba6c2714
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-19 20:01:45 -0500

    Crear enlaces de categorias y tipos para el header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,56 @@
 
             <b-navbar-nav>
                 <b-nav-item href="#">Home</b-nav-item>
+
                 <b-nav-item-dropdown text="User" right>
                     <b-dropdown-item href="#">Account</b-dropdown-item>
                     <b-dropdown-item href="#">Settings</b-dropdown-item>
                 </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown text="Tipos" right>
+                    <b-dropdown-item  v-for="t in types" v-bind:key="t.id" :to=" '/type/' + t.id +'/element'">
+                        {{ t.title }}
+                    </b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+
+
+                <b-nav-item-dropdown text="Categorias" right>
+                    <b-dropdown-item  v-for="c in categories" v-bind:key="c.id" :to=" '/category/' + c.id +'/element'">
+                        {{ c.title }}
+                    </b-dropdown-item>
+                    </b-nav-item-dropdown>
             </b-navbar-nav>            
         </b-navbar>
     </div>
 </template>
+
+
+
+
+<script>
+export default {
+    created(){
+        this.findAllType();
+        this.findAllCategory();
+    },
+    data(){
+        return{
+            types: [],
+            categories: []
+        };
+    },
+    methods: {
+        findAllType: function(){
+            fetch('http://127.0.0.1:8000/api/type/?format=json') 
+                .then(res => res.json())
+                .then(res => this.types = res)                
+        },
+        findAllCategory: function(){
+            fetch('http://127.0.0.1:8000/api/category/?format=json') 
+                .then(res => res.json())
+                .then(res => this.categories = res)                
+        }
+    },
+}
+</script>
