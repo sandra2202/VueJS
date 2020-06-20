10da4eed5c2fc2a3c8d3fd20035ca0b946585344
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-19 22:16:22 -0500

    Crear componente generico de listado de elementos

--- src/components/ListCategory.vue
+++ src/components/ListCategory.vue
@@ -1,32 +1,23 @@
 <template>
     <div class="container">
-        Listado de Categorias 
-        <div v-for="e in elements" v-bind:key="e.id">
-            <router-link :to=" '/detail/' + e.id ">
-                <b-card
-                    :title="e.title"
-                >
-
-                <b-card-text>
-                    {{ e.description }}
-                </b-card-text> 
-
-                </b-card>      
-            </router-link>
-        </div>
-
+        <ListDefault :elements-list="elements"/>
     </div>
 </template>
 
 <script>
+
+import ListDefault from '../partials/_ListDefault'
+
 export default {
 
+    components: {
+        ListDefault
+    },
     created(){
         this.findAll()
     },
     data(){
         return{
-            msj: "... :(",
             elements: []
         };
     },
---
+++ src/partials/_ListDefault.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div v-for="e in elementsList" v-bind:key="e.id">
+            <router-link :to=" '/detail/' + e.id ">
+                <b-card :title="e.title">
+                    <b-card-text>{{ e.description }}</b-card-text> 
+                </b-card>      
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props:[
+        'elementsList'
+    ],
+    data(){
+        return{
+            elements: []
+        };
+    },
+    methods: {             
+        
+    },
+}
+</script>
+
+<style>
+    .box{
+        border: 3px solid #CCC;
+        margin: 5px 0 0 0;
+    }
+    
+</style>
