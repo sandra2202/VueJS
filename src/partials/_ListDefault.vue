d3e2612a361349be6183b2029da32b90283a314d
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-20 19:00:05 -0500

    Mejorar el componente de crata para el listado de elementos

--- src/partials/_ListDefault.vue
+++ src/partials/_ListDefault.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
+    <div class="mt-3 mt-md-0">
         <div v-for="e in elementsList" v-bind:key="e.id">
-            <router-link :to=" '/detail/' + e.id ">
-                <b-card :title="e.title">
+            <router-link class="card-link" :to=" '/detail/' + e.id ">
+                <b-card :header="e.title"
+                header-bg-variant="primary"
+                header-text-variant="white">
                     <b-card-text>{{ e.description }}</b-card-text> 
                 </b-card>      
             </router-link>
@@ -28,9 +30,13 @@ export default {
 </script>
 
 <style>
-    .box{
-        border: 3px solid #CCC;
-        margin: 5px 0 0 0;
+    a.card-link,
+    a.card-link:hover {
+        color: #ff3399;
+        text-decoration: none;
+    }
+    a.card-link:hover .card-body {
+        background: #f8f9fa;
     }
     
 </style>
