eb6b64400f24a84e81f4b1d4111d16b03b917bec
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-20 20:09:33 -0500

    Hacer peticiones para obtener detalles de la categoria y tipo

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -3,8 +3,8 @@
         <b-card>
             <h1>{{ element.title }}</h1>
             <div class="p-3">
-                <router-link class="btn btn-danger btn-sm" :to="{ name:'list-category',params:{id: element.category} }">Categoría</router-link>
-                <router-link class="ml-2 btn btn-danger btn-sm" :to="{ name:'list-type',params:{id: element.type} }">Type</router-link>
+                <router-link class="btn btn-danger btn-sm" :to="{ name:'list-category',params:{id: element.category} }">{{ category.title }}</router-link>
+                <router-link class="ml-2 btn btn-danger btn-sm" :to="{ name:'list-type',params:{id: element.type} }">{{ type.title }}</router-link>
                 <b-card-text>{{ element.description }}</b-card-text>
             </div>             
         </b-card>      
@@ -18,14 +18,30 @@ export default {
     },
     data(){
         return{
-            element: Object
+            element: Object,
+            category: Object,
+            type: Object
         };
     },
     methods: {
         find: function(){
             fetch("http://127.0.0.1:8000/api/element/"+this.$route.params.id+"/?format=json") 
                 .then(res => res.json())
-                .then(res => this.element = res)                
+                .then(res => {
+                    this.element = res
+                    this.findCategory(this.element.category);
+                    this.findType(this.element.type);
+                });                
+        },
+        findCategory: function(id){
+            fetch("http://127.0.0.1:8000/api/category/"+id+"/?format=json") 
+                .then(res => res.json())
+                .then(res => (this.category = res))               
+        },
+        findType: function(id){
+            fetch("http://127.0.0.1:8000/api/type/"+id+"/?format=json") 
+                .then(res => res.json())
+                .then(res => (this.type = res))               
         }
     },
 }
